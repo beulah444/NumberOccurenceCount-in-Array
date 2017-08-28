2bda70f83a5c6f4b3e17523e25e6bb4b118d845e
beulah444 <beulah444@gmail.com>
2017-08-28 16:57:28 -0700

    Add project files.

---
+++ NumberOccurenceCount-in-Array/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main()
+    {
+        int[] sortedarray = new int[] { 1, 2, 3, 5, 5,5, 12, 90, 120, 169, 671 };
+        Console.WriteLine("enter number");
+        bool isNum = int.TryParse(Console.ReadLine(), out int result);
+        if (isNum)
+        {
+            int count = 0;
+            foreach (int i in sortedarray)
+            {
+                if(i==result)
+                {
+                    count++;
+                }
+
+            }
+            if (count == 1)
+            { Console.WriteLine("The number {0} has occurred {1}  times in the array", result, count); }
+            else
+            { Console.WriteLine("The number {0} is not present in the array", result); }
+            
+        }
+        else
+        {
+            Console.WriteLine("Invalid number");
+        }
+        Console.ReadKey();
+    }
+}
